7a5434b1be2aebb9714f9058a5ba69d338f18f93
juan.monino <juan.monino@sapiens.com>
2023-11-28 13:10:07 +0100

    Initial commit

---
+++ src/main/java/com/juanlumn/gptintegration/GptintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.juanlumn.gptintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GptintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GptintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juanlumn/gptintegration/GptintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanlumn.gptintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GptintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
